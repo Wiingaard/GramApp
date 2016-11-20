49cde64e20e9b204b07b813f673b4ad999d9c986
Martin Wiingaard <martin.wiin@gmail.com>
2016-11-20 21:19:39 +0100

    Added Xlsx-framework with Bridgeing header to swift. Changed deployment targer to iOS 9.0

--- GramApp/ViewController.swift
+++ GramApp/ViewController.swift
@@ -12,14 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        simpleTest()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func simpleTest() {
+        
+        // Load worksheet
+        let loadPath = Bundle.main.path(forResource: "simpleTemplate", ofType: "xlsx")
+        let spreadsheet: BRAOfficeDocumentPackage = BRAOfficeDocumentPackage.open(loadPath)
+        var worksheet: BRAWorksheet = spreadsheet.workbook.worksheets[0] as! BRAWorksheet
+        
+        // Add Image
+        let image: UIImage = UIImage(named: "awesome-face.png")!
+        var drawing: BRAWorksheetDrawing = worksheet.add(image, inCellReferenced: "B2", withOffset: CGPoint(x: 30, y: 30), size: image.size, preserveTransparency: true)
+        drawing.insets = UIEdgeInsetsMake(0.0, 0.0, 0.5, 0.5)
+        
+        // Save worksheet
+        var savePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last!
+        savePath += "/template-saved.xlsx"
+        spreadsheet.save(as: savePath)
+        
     }
-
-
 }
 
