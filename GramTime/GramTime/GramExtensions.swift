2697bf0e449c828f8eb219bd7dd486e9eb01bd02
Martin Wiingaard <martin@donkeyrepublic.com>
2017-01-10 22:58:30 +0100

    !!! Pasted to new project  !!! To avoid 90035 error when uploading to Itunes Connect. Changed images to assets, and removed calls to "Image"

---
+++ GramTime/GramTime/GramExtensions.swift
@@ -0,0 +1,50 @@
+//
+//  GramExtensions.swift
+//  GramTime
+//
+//  Created by Martin Wiingaard on 10/01/2017.
+//  Copyright © 2017 Gram Equipsment AS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension NSDate {
+    /**
+     *  Nedrunder NSDate til næsmester 5 minut
+     */
+    func roundedTime() -> NSDate {
+        let seconds = floor(self.timeIntervalSince1970/(300))*300
+        return NSDate(timeIntervalSince1970: seconds)
+    }
+}
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    /// Init a UIColor form a 6-digit hex value, like: 0x7ADC7B
+    convenience init(hexInt: Int) {
+        self.init(red:(hexInt >> 16) & 0xff, green:(hexInt >> 8) & 0xff, blue:hexInt & 0xff)
+    }
+    
+    static var gramRed: UIColor { return UIColor(hexInt: 0xF96262) }
+    static var gramGreen: UIColor { return UIColor(hexInt: 0x86C289) }
+}
+
+extension UIImage {
+    convenience init(view: UIView) {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.init(cgImage: (image?.cgImage)!)
+    }
+}
+
---
+++ GramTime/GramTime/ProjectStatusViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ProjectStatusViewController.swift
+//  GramApp
+//
+//  Created by Martin Wiingaard on 11/12/2016.
+//  Copyright © 2016 Fiks IVS. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ProjectStatusViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func nextAction(_ sender: Any) {
+        try! realm.write {
+            report.completedStatus = self.getCompletedStatus()
+        }
+        performSegue(withIdentifier: "Show Sign And Send", sender: nil)
+    }
+    
+    // Model:
+    var reportID = ""       // should be overriden from segue
+    let realm = try! Realm()
+    var report: WeekReport!
+    var selected = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let reportIDPredicate = NSPredicate(format: "reportID = %@", reportID)
+        report = realm.objects(WeekReport.self).filter(reportIDPredicate).first!
+        
+        let nib = UINib(nibName: "CheckmarkTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "CheckmarkTableViewCell")
+        
+        selected = report.completedStatus ? 1 : 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        switch report.completedStatus {
+        case false:
+            setCheckmark(at: 0)
+            selected = 0
+        case true:
+            setCheckmark(at: 1)
+            selected = 1
+        }
+    }
+    
+    func getCompletedStatus() -> Bool{
+        switch selected {
+        case 0:
+            return false
+        case 1:
+            return true
+        default:
+            fatalError("default not allowed")
+        }
+    }
+    
+    // MARK: - Table view
+    var notCompletedCell: CheckmarkTableViewCell!
+    var completedCell: CheckmarkTableViewCell!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.row {
+        case 0:
+            notCompletedCell = tableView.dequeueReusableCell(withIdentifier: "CheckmarkTableViewCell") as! CheckmarkTableViewCell
+            notCompletedCell.titleLabel.text = "Not completed yet"
+            return notCompletedCell
+        case 1:
+            completedCell = tableView.dequeueReusableCell(withIdentifier: "CheckmarkTableViewCell") as! CheckmarkTableViewCell
+            completedCell.titleLabel.text = "Completed"
+            return completedCell
+        default:
+            fatalError("default not allowed!")
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        setCheckmark(at: indexPath.row)
+        selected = indexPath.row
+    }
+    
+    func setCheckmark(at indexpathRow: Int) {
+        switch indexpathRow {
+        case 0:
+            notCompletedCell.accessoryType = .checkmark
+            completedCell.accessoryType = .none
+        case 1:
+            notCompletedCell.accessoryType = .none
+            completedCell.accessoryType = .checkmark
+        default:
+            fatalError("default not allowed!")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Show Sign And Send" {
+            let vc = segue.destination as! SignAndSendViewController
+            vc.reportID = self.reportID
+        }
+    }
+}
